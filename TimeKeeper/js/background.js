2bf45e84e29ceae6aeedae94adebaca6e81c7bdb
Max G. Hegler <max@maxhegler.com>
2015-02-03 16:03:20 -0800

    misc

--- TimeKeeper/js/background.js
+++ TimeKeeper/js/background.js
@@ -1,8 +1,10 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('../window.html', {
-    'bounds': {
-      'width': 430,
-      'height': 600
-    }
-  });
+	chrome.app.window.create('../window.html', {
+		'bounds': {
+			'width': 430,
+			'height': 600,
+			'left': 0,
+			'top': 0
+		}
+	});
 });
