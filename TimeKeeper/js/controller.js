214e53f38953026590710691b4fac2b0477e90a5
Max G. Hegler <max@maxhegler.com>
2015-06-29 14:21:43 -0700

    basic css additions

--- TimeKeeper/js/controller.js
+++ TimeKeeper/js/controller.js
@@ -1,25 +1,6 @@
 // Initialize the Controller
-var TimeKeeperController = TimeKeeper.controller('TimeKeeperController', function($scope, $interval, Storage) {
+var TimeKeeperController = TimeKeeper.controller('TimeKeeperController', function($scope, Storage) {
 
-	var start_time = 0;
-	var invterval = undefined;
-
-	$scope.time = 0;
-	$scope.start = function() {
-		start_time = Date.now();
-
-		invterval = $interval(function() {
-
-			$scope.time = Date.now() - start_time;
-
-		}, 1000);
-	};
-
-	$scope.stop = function() {
-		$interval.cancel(interval);
-		interval = undefined;
-	};
-
-	$scope.start();
+	
 
 });
