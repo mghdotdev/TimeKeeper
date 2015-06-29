cf04a83ad9c0cef445e8ba8194b363e287f3987f
Max G. Hegler <max@maxhegler.com>
2015-06-29 12:26:55 -0700

    Timer Test

--- TimeKeeper/js/controller.js
+++ TimeKeeper/js/controller.js
@@ -1,6 +1,25 @@
 // Initialize the Controller
-TimeKeeper.controller('TimeKeeperController', function($scope, Storage) {
+var TimeKeeperController = TimeKeeper.controller('TimeKeeperController', function($scope, $interval, Storage) {
 
-	
+	var start_time = 0;
+	var invterval = undefined;
+
+	$scope.time = 0;
+	$scope.start = function() {
+		start_time = Date.now();
+
+		invterval = $interval(function() {
+
+			$scope.time = Date.now() - start_time;
+
+		}, 1000);
+	};
+
+	$scope.stop = function() {
+		$interval.cancel(interval);
+		interval = undefined;
+	};
+
+	$scope.start();
 
 });
