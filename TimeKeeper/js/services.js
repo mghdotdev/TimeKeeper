507c37a463ccd3a5f4d2160a548d0536d152cfaa
Max G. Hegler <max@maxhegler.com>
2015-06-29 12:26:43 -0700

    Timer Test

--- TimeKeeper/js/services.js
+++ TimeKeeper/js/services.js
@@ -6,37 +6,36 @@ var tkServices = angular.module('TimeKeeper.services', []);
  * ==============================
 
 	1)	Local Storage Factory
+		@ CREDITS: SimplyDo (https://github.com/SimplyDo/projector)
 		- Handles storing/retriving saved JSON objects from TimeKeeper
 		- Supplies a method for checking the browsers localStorage compatibility
 
-	2)	Timer Factory
-
 */
 
-tkServices.factory('Storage', function() {
+tkServices.factory('Storage', ['$window', function($window) {
 
-	var instance = {};
+	var $instance = {};
 
-	instance.loadObject = function(key) {
+	$instance.loadObject = function(key) {
 		var data = [];
 
-		var json_object = window.localStorage[key];
+		var json_object = $window.localStorage.getItem(key);
 		if (json_object) {
 			data = JSON.parse(json_object);
 		}
 
 		return data;
 	};
-	instance.saveObject = function(obj, key) {
-		window.localStorage[key] = JSON.stringify(obj);
+	$instance.saveObject = function(obj, key) {
+		$window.localStorage.setItem(key, JSON.stringify(obj));
 	};
-	instance.clear = function() {
-		window.localStorage.clear();
+	$instance.clear = function() {
+		$window.localStorage.clear();
 	};
-	instance.supported = function() {
-		return 'localStorage' in window && window['localStorage'] !== null;
+	$instance.supported = function() {
+		return 'localStorage' in $window && $window['localStorage'] !== null;
 	};
 
-	return instance;
+	return $instance;
 
-});
+}]);
