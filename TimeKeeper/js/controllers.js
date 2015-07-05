2fb2e81a6cb80891f32bc6375829b179a441bccf
Max G. Hegler <max@maxhegler.com>
2015-07-05 11:38:48 -0700

    Filters

--- TimeKeeper/js/controllers.js
+++ TimeKeeper/js/controllers.js
@@ -1,12 +1,5 @@
-TimeKeeper.controller('ProjectController', ['$scope', 'Storage', function($scope, Storage) {
+TimeKeeper.controller('FilterTest', ['$scope', '$window', function($scope, $window) {
 
-	// Load in 'tk-projects' localStorage data : else []
-	$scope.projects = Storage.loadObject('tk-projects');
-
-	// ---- Methods ---- //
-	$scope.addProject = function() {
-		var p = { name: $scope.projectName };
-		$scope.projects.push(p);
-	};
+	$scope.time = 1440000;
 
 }]);
